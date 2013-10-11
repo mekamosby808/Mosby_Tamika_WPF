e717ecca1ba8c95049a6270ea147bb3b7679cae3
meka808 <mekamosby@yahoo.com>
2013-10-10 18:28:39 -0700

    ClassExample_WorksheetExmple
    Going over student question on worksheet

--- 1013-WPFClassExamples/js/example.js
+++ 1013-WPFClassExamples/js/example.js
@@ -6,25 +6,30 @@
 
 //Variables//
 
-var shoeBoxVariable; // Declares a variable that declares a variable named shoeBoxVariable
-shoeBoxVariable=5;  // sets the variable shoeBoxVariable equal 5 (Uses 1 "=")
-console.log(shoeBoxVariable);
+//var shoeBoxVariable; // Declares a variable that declares a variable named shoeBoxVariable
+//shoeBoxVariable=5;  // sets the variable shoeBoxVariable equal 5 (Uses 1 "=")
+//console.log(shoeBoxVariable);
 
 
 // combining prompts and console.logs
 
-var userData;
-var userData = prompt('How Old Are You?');
-console.log(userData);
-var userNumberOfSiblings=prompt('Number of Siblings?');  // return number value
-
-
-
-var productUserData = userData*userNumberOfSiblings;
-console.log(userData*userNumberOfSiblings);
-
-var userFirstName = prompt('What is your first Name?');
-var userLastName = prompt('What is your Last Name?');
-
-console.log (userFirstName+userLastName);
-
+//var userData;
+////var userData = prompt('How Old Are You?');
+////console.log(userData);
+//
+//var userNumberOfSiblings=prompt('Number of Siblings?');  // return number value
+//
+//
+//
+////var productUserData = userData*userNumberOfSiblings;
+////console.log(userData*userNumberOfSiblings);
+//
+//var userFirstName = prompt('What is your first Name?');
+//var userLastName = prompt('What is your Last Name?');
+//
+//console.log (userFirstName+userLastName);
+//console.log(userFirstName+'\t'+userLastName+' '+userData);
+//console.log(userData+userNumberOfSiblings);
+//
+//userData=userData*1;
+//userData+Number(userData);
