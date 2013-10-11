02ad34cecdcdf529f89a985e78e979a5cc2a68ef
meka808 <mekamosby@yahoo.com>
2013-10-10 17:50:45 -0700

    SampleCodeVariableLogs_Prompts

--- 1013-WPFClassExamples/js/example.js
+++ 1013-WPFClassExamples/js/example.js
@@ -1,3 +1,30 @@
 /**
  * Created by Meka808 on 10/10/13.
  */
+//prompt("What color is your cat?");
+//console.log("a");
+
+//Variables//
+
+var shoeBoxVariable; // Declares a variable that declares a variable named shoeBoxVariable
+shoeBoxVariable=5;  // sets the variable shoeBoxVariable equal 5 (Uses 1 "=")
+console.log(shoeBoxVariable);
+
+
+// combining prompts and console.logs
+
+var userData;
+var userData = prompt('How Old Are You?');
+console.log(userData);
+var userNumberOfSiblings=prompt('Number of Siblings?');  // return number value
+
+
+
+var productUserData = userData*userNumberOfSiblings;
+console.log(userData*userNumberOfSiblings);
+
+var userFirstName = prompt('What is your first Name?');
+var userLastName = prompt('What is your Last Name?');
+
+console.log (userFirstName+userLastName);
+
