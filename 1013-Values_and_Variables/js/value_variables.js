bbceb57519a0d57a00d7527721237303184c8b2c
meka808 <mekamosby@yahoo.com>
2013-10-11 19:38:48 -0700

    Value and Variables video
    followed along with video, comments of where start and end point are
    listed.

---
+++ 1013-Values_and_Variables/js/value_variables.js
@@ -0,0 +1,20 @@
+/**
+ * Created by Meka808 on 10/11/13.
+ */
+
+
+//    Video Value and Variables Video Start
+var yearBorn = 1955;
+
+
+console.log(yearBorn);
+console.log("Hello World");
+
+alert("Hello World");
+
+//End of Video
+
+//Declaring Variables Start
+
+
+
