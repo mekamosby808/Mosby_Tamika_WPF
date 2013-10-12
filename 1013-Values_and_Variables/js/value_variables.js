32894ce4b5acf74957bd875928b808c1dcf53884
meka808 <mekamosby@yahoo.com>
2013-10-11 20:22:17 -0700

    Arrays example
    talked about arrays and array access notation and how to make sure to
    be aware of the sequence.

--- 1013-Values_and_Variables/js/value_variables.js
+++ 1013-Values_and_Variables/js/value_variables.js
@@ -3,30 +3,30 @@
  */
 
 
-//    Video Value and Variables Video Start
+////    Video Value and Variables Video Start
 var yearBorn = 1955;
-
-
+//
+//
 console.log(yearBorn);
 console.log("Hello World");
 
 alert("Hello World");
-
-//End of Video
-
-
-// Defining Variables example Start
-
-var age; 
+//
+////End of Video
+//
+//
+//// Defining Variables example Start
+//
+var age;
 age = 38;
 var yearOfBirth = 1970;
 
-//End of Defining Variables
-
-
-
-//String and Boolean example
-
+////End of Defining Variables
+//
+//
+//
+////String and Boolean example
+//
 var age = 38;
 var isStudent = false;
 var firstName = "Meka";
@@ -34,4 +34,9 @@ var phrase = "I don\'t know \n you\'ll have to speak with the professor"
 
 // End of String Booleans Video example
 
+// Arrays example
 
+var muppetNames = ["Kermit", "Miss Piggy", "Gonzo","Rizzo,"]; // 0,1,2,3
+console.log(muppetNames[0]);
+
+// End of Arrays example
