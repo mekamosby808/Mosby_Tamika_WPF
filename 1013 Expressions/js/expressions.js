f68ed715722ac0fca0e82360f75f1cac3dc56048
meka808 <mekamosby@yahoo.com>
2013-10-11 20:43:52 -0700

    Expressions_Tamika Mosby
    I was having a hard time with usderstanding why I wasn't getting the
    same console.log output as the video. Could you please explain to me
    what I was doing wrong. I was trying to chec the red box on the right
    to see what error I was getting. This is also commented in the file.

---
+++ 1013 Expressions/js/expressions.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Meka808 on 10/11/13.
+ */
+
+// Basic Expressions
+
+var a = 2;
+var a = a + 3;
+console.log(a);
+
+
+// I was not getting the same console.log output as in the video, I tried to figure out what the problem was but didn't see to get it. 
