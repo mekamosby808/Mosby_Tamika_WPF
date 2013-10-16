8d326e61dcf8d75747da4dc0e35bd999539a1daf
meka808 <mekamosby@yahoo.com>
2013-10-16 14:44:57 -0700

    Last chance for Gas
    I think I did this one right I didn't know if I needed to use all of
    the givens

--- Conditionals_Worksheet/js/conditionals.js
+++ Conditionals_Worksheet/js/conditionals.js
@@ -6,22 +6,28 @@
 //is the competitor more than 250lbs? yes
 //do they qualify? yes
 
-
-var competitorWeight = 300;
-
-
-
-if(competitorWeight < 250) {
-console.log("The competitor qualifies for the heavyweight division");
-    }
-else{("if the competitor is > than 250lbs")
-console.log("The competitor needs to gain some weight");}
+//
+//var competitorWeight = 300;
+//
+//
+//
+//if(competitorWeight < 250) {
+//console.log("The competitor qualifies for the heavyweight division");
+//    }
+//else{("if the competitor is > than 250lbs")
+//console.log("The competitor needs to gain some weight");}
 
 //Last chance for Gas
 
-//var milesPG = 40;
-//var gaugeReading = 50;
-//var tankInGallons = 20;
-//
-//console.log("Yes you can make it without stopping for gas");
-//
+
+var gaugeReading =  50;
+//if tank is half full then you have enough gas to continue on
+//if tank is less than half full, you need to stop and get gas
+
+if(gaugeReading <= 50){
+    console.log("Yes you can make it without stopping for gas");
+}
+
+else{("if the tank is > than .50")
+    console.log("You need to stop for gas, you only have 10 gallons of gas")
+}
