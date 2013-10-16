2dc814c30900bd463a79c19ea3ab62fd9208563f
meka808 <mekamosby@yahoo.com>
2013-10-16 14:17:10 -0700

    Stuffing your face
    I changed a few things I figured out what the problem was and I am
    still working on the rest of the problem

--- Conditionals_Worksheet/js/conditionals.js
+++ Conditionals_Worksheet/js/conditionals.js
@@ -3,12 +3,25 @@
  */
 
 //if the competitor's weight is more than 250lbs than you can enter the contest
+//is the competitor more than 250lbs? yes
+//do they qualify? yes
 
-var competitorWeight = true;
+
+var competitorWeight = 300;
 
 
 
-if(competitorWeight){"is more that 250lbs"}
-                                            //if the competitor's weight is less than 250lbs than they cannot enter
-else{"is less than 250lbs"}("you cannot enter the contest")
-console.log("You can enter the Contest");
+if(competitorWeight < 250) {
+console.log("The competitor qualifies for the heavyweight division");
+    }
+else{("if the competitor is > than 250lbs")
+console.log("The competitor needs to gain some weight");}
+
+//Last chance for Gas
+
+//var milesPG = 40;
+//var gaugeReading = 50;
+//var tankInGallons = 20;
+//
+//console.log("Yes you can make it without stopping for gas");
+//
