186cf3312ed216b24ef20816f7d96e566630c8ea
meka808 <mekamosby@yahoo.com>
2013-10-19 20:08:00 -0700

    Function Screencast
    output message for calculating the area

---
+++ Functions_Screencast/js/functions_screencast.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Meka808 on 10/19/13.
+ */
+
+
+//Functions Screencasts Tamika Mosby
+
+function outptMsg(){
+    console.log("Hello World");
+}
+
+function calcArea(){
+    var width = 20;
+    var height = 30;
+    var area = width * height;
+    console.log(area);
+}
